c51449b1f867538c30b9a3da083547ecf66986a2
Daniela Estrada <danniestrada@me.com>
2023-11-30 12:39:57 +0100

    SQL Relación one-to-one

--- 004.sql/009.one-to-one.sql
+++ 004.sql/009.one-to-one.sql
@@ -29,7 +29,27 @@ INSERT INTO estudiantes (nombre, fecha_nacimiento, email) VALUES
 ('Javier Ramirez', '1989-11-25', 'ejemplo4@gmail.com'),
 ('Laura Hernandez', '2015-11-25', 'ejemplo5@gmail.com');
 
+INSERT INTO cursos(nombre, estudiante_id) VALUES
+('Inglés', 3),
+('Ciencias', 4),
+('Economía', 5), 
+('Arte', 1),
+('Física', 2);
 
+-- Consulta de los estudiantes que nacieron antes del 2015
+SELECT * FROM estudiantes WHERE fecha_nacimiento < '2015-01-01';
+
+-- Contar el número total de estudiantes
+SELECT COUNT(*)
+FROM estudiantes;
+
+-- Actualizar el email de el estudiante con id= 2
+UPDATE estudiantes SET email= 'correomodificado@gmail.com' WHERE estudiante_id=2;
+
+UPDATE estudiantes SET fecha_nacimiento = '2001-03-10' WHERE nombre= 'Maria Lopez';
+
+-- Eliminar a todos los estudiantes que nacieron después del 2015
+DELETE FROM estudiantes WHERE fecha_nacimiento > '2015-01-01';
 
 
 
