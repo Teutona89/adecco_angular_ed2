10bc45971def8e4aa4600db744659fee56cf7ece
alansastre <alan@alansastre.co>
2023-12-12 11:00:52 +0100

    Python operadores

---
+++ 005.python/006.operadores_aritmeticos.py
@@ -0,0 +1,8 @@
+
+
+print(50 / 35) # División exacta 1.4705882352941178
+print(round(50 / 35, 2)) # Redondear el resultado a 2 decimales
+
+print(50 // 35) # División entera 1, elimina la parte decimales
+
+print(2 ** 3) # Exponente
