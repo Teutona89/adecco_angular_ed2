e0d49df0372900fbde94a981da459df3d41011ea
alansastre <alan@alansastre.co>
2023-12-12 13:06:41 +0100

    Python operadores lógicos

---
+++ 005.python/008.operadores_logicos.py
@@ -0,0 +1,30 @@
+
+# Operadores lógicos permiten combinar varias condiciones
+
+# and equivalente a Y
+# Restrictivo obliga a que las condiciones sean todas verdaderas
+
+usuario_correcto = "admin"
+password_correcta = "1234"
+
+usuario = input('Introduce tu usuario: ')
+password = input('Introduce tu contraseña: ')
+
+if usuario == usuario_correcto and password == password_correcta:
+    print('Las credendiales son correctas')
+else:
+    print('Credenciales incorrectas')
+    
+# or equivalente O
+# Flexible, con que una de las condiciones sea True el resultado es True
+
+es_estudiante = input('¿Es estudiante? (si/no): ').lower() == 'si'
+precio_total = float(input('Introduce precio de compra: '))
+
+if es_estudiante or precio_total > 100:
+    # precio_total = precio_total * 0.80 # quitar 20 %
+    # precio_total *= 0.80
+    precio_total = round(precio_total * 0.80, 2)
+    print(f'Tienes un descuento, precio: {precio_total}')
+else:
+    print(f'No tienes descuento, precio: {precio_total}')
