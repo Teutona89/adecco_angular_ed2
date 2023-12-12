86edf5fd850f6df25b3c424229bdf9f238dff139
alansastre <alan@alansastre.co>
2023-12-12 11:44:20 +0100

    Python operadores aritméticos

--- 005.python/006.operadores_aritmeticos.py
+++ 005.python/006.operadores_aritmeticos.py
@@ -5,4 +5,13 @@ print(round(50 / 35, 2)) # Redondear el resultado a 2 decimales
 
 print(50 // 35) # División entera 1, elimina la parte decimales
 
+# Ejemplo de uso división entera en un contexto en el que los decimales no tiene
+total_regalos = 123
+total_mesas = 27
+
+regalos_por_mesa = total_regalos // total_mesas
+
+# f string: string formateado, es un string que permite incrustar una variable dentro
+print(f"Cada mesa recibirá {regalos_por_mesa} regalos.")
+
 print(2 ** 3) # Exponente
