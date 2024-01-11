432b1a297209bb1d20ffe5e504f42640dac0c32d
alansastre <alan@alansastre.co>
2024-01-11 12:03:41 +0100

    Java indexOf y set en ArrayList

--- 006.java/006.estructuras_datos/ArrayListEjemplo3.java
+++ 006.java/006.estructuras_datos/ArrayListEjemplo3.java
@@ -6,16 +6,21 @@ public class ArrayListEjemplo3 {
         // Crear una lista de productos
         ArrayList<String> productos = new ArrayList<>();
         productos.add("Carcasa iPad");
-        productos.add("Teléfono fijo Motorola");
+        productos.add("Telefono fijo Motorola");
         productos.add("iPhone 15");
         productos.add("Xiaomi 13 Pro");
-        
+
         // modificar el producto llamado iPhone 15 y ponerle iPhone 15T Plus
 
-        // indexOf
-        // set
+        // Averiguar el índice/posición del elemento "iPhone 15" utilizando indexOf
+        int index = productos.indexOf("iPhone 15");
+        
+        // Modificar el elemento "iPhone 15" utilizando set
+        String nuevoIPhone = "iPhone 15T Plus";
+        String antiguoIPhone = productos.set(index, nuevoIPhone);
 
-
-
+        for (String producto : productos) {
+            System.out.println(producto);
+        }
     }
 }
