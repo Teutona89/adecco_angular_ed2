2f1ae83d6f623b8b1e6114372503e7d3e31fe2c6
alansastre <alan@alansastre.co>
2024-01-11 13:01:19 +0100

    Java esquema app consola

---
+++ 006.java/008.app/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+        
+        // while true
+
+        // Mostrar menú opciones
+        String menu = """
+            1 - Mostrar productos
+            2 - Mostrar un producto por id
+            3 - Introducir nuevo producto
+            4 - Actualizar un producto
+            5 - Borrar un producto
+            6 - Salir
+            """;
+        System.out.println(menu);
+
+        // Pedir y Leer opción por consola
+
+        // switch comprobar la opción y ejecutar un método distinto para cada opción
+
+        // break salir del bucle
+    }
+}
