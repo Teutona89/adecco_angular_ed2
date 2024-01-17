5010a4524973095d256c1924481ea6f37e285ae4
alansastre <alan@alansastre.co>
2024-01-17 11:02:34 +0100

    JavaScript Clase Evento

---
+++ 007.javascript/date/evento.js
@@ -0,0 +1,26 @@
+
+// new Date()
+
+// Crear una clase Evento
+// atributos: title, price, startDate, finishDate, participantsNum
+
+
+// Crear objetos con fecha
+
+class Evento {
+
+    constructor(title, price, startDate, finishDate, participantsNum) {
+        this.title = title;
+        this.price = price;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+        this.participantsNum = participantsNum;
+    }
+}
+
+/*
+let fechaInicio = ...
+let fechaFin = ...
+
+let techFest = new Evento
+*/
