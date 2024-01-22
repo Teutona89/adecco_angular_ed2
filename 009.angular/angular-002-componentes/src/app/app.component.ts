beb66400e1d15f0716e6ff99cba9d103d6585c24
alansastre <alan@alansastre.co>
2024-01-22 13:52:58 +0100

    Angular componente clientes

--- 009.angular/angular-002-componentes/src/app/app.component.ts
+++ 009.angular/angular-002-componentes/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductosComponent } from "./productos/productos.component";
+import { ClientesComponent } from './clientes/clientes.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [ProductosComponent]
+    imports: [ProductosComponent, ClientesComponent]
 })
 export class AppComponent {
   title = 'angular-002-componentes';
