0565f54c36b431973d392c9527a875c714380252
alansastre <alan@alansastre.co>
2024-01-23 12:45:07 +0100

    Angular directiva for

--- 009.angular/angular-003-directivas/src/app/app.component.ts
+++ 009.angular/angular-003-directivas/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [ProductListComponent]
 })
 export class AppComponent {
   title = 'angular-003-directivas';
