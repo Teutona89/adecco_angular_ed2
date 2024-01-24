447d202e22b0e6a34473239f48afb4bab30a4e00
alansastre <alan@alansastre.co>
2024-01-24 10:34:23 +0100

    Angular (click) en button

--- 009.angular/angular-003-directivas/src/app/app.component.ts
+++ 009.angular/angular-003-directivas/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [ProductListComponent]
+    imports: [ProductListComponent, ProductDetailComponent]
 })
 export class AppComponent {
   title = 'angular-003-directivas';
