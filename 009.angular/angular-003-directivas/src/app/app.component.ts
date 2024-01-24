38a8359302f7ca1620687e01f1062d8ed24e2504
alansastre <alan@alansastre.co>
2024-01-24 12:10:13 +0100

    Angular componente contador

--- 009.angular/angular-003-directivas/src/app/app.component.ts
+++ 009.angular/angular-003-directivas/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ContadorComponent } from "./contador/contador.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [ProductListComponent, ProductDetailComponent]
+    imports: [ProductListComponent, ProductDetailComponent, ContadorComponent]
 })
 export class AppComponent {
   title = 'angular-003-directivas';
---
+++ 009.angular/angular-003-directivas/src/app/contador/contador.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [],
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+
+  // Crear variable contador number sin valor
+  
+
+  // método para incrementar
+
+  // método para decrementar
+
+  // método para resetear a 0
+}
