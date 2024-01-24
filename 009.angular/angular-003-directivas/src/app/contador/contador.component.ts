9adce887f1d0b2b9269b3499a26dc8bb219bf1b1
alansastre <alan@alansastre.co>
2024-01-24 12:38:29 +0100

    Angular contador método incrementar

--- 009.angular/angular-003-directivas/src/app/contador/contador.component.ts
+++ 009.angular/angular-003-directivas/src/app/contador/contador.component.ts
@@ -8,12 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './contador.component.css'
 })
 export class ContadorComponent {
-
   // Crear variable contador number sin valor
-  
-
+  contador: number | undefined;
   // método para incrementar
-
+  incrementar() {
+    if(!this.contador) {
+      this.contador = 0;
+    }
+    this.contador++;
+  }
   // método para decrementar
 
   // método para resetear a 0
