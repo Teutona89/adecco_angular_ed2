4fc6578fe02cdd3d397359aaf2f3cb71de0ff44f
alansastre <alan@alansastre.co>
2024-01-25 13:55:03 +0100

    Angular tabla bootstrap con @for

--- 009.angular/angular-004-bootstrap/src/app/app.component.ts
+++ 009.angular/angular-004-bootstrap/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { UserListComponent } from "./user-list/user-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [UserListComponent]
 })
 export class AppComponent {
   title = 'angular-004-bootstrap';
