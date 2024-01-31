00c3c54ae96b009a9874951d6aa3f76d0a3e54ec
alansastre <alan@alansastre.co>
2024-01-31 12:59:50 +0100

    Angular componente home

--- 009.angular/angular-005-enrutado/src/app/app.routes.ts
+++ 009.angular/angular-005-enrutado/src/app/app.routes.ts
@@ -4,8 +4,30 @@ import { RestaurantFormComponent } from './restaurant-form/restaurant-form.compo
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantBookingComponent } from './restaurant-booking/restaurant-booking.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  /* Opción 1: redirigir a un listado
+  CASO ESPECIAL: se pone pathMatch full para evitar que detecte cualquier url
+  ya que todas las urls empiezan por cadena vacía
+  Restringir la detección de la cadena vacía si y solo si está vacía
+  */
+  /*
+  {
+    path: '',
+    redirectTo: '/restaurantes',
+    pathMatch: 'full'
+  },
+  */
+
+  /*
+  OPCIÓN 2: crear un componente HOME que sea la pantalla de inicio desde donde navegar a los listados
+  ng generate component home
+  */
+  {
+    path: '', // ruta vacía
+    component: HomeComponent
+  },
   {
     path: 'restaurantes',
     component: RestaurantListComponent
