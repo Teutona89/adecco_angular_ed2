a9e10b11a222f0c8bbdb0b393d08f342a5478543
alansastre <alan@alansastre.co>
2024-02-01 10:35:24 +0100

    Angular restaurant-detail grid de atributos

--- 009.angular/angular-005-enrutado/src/app/restaurant-detail/restaurant-detail.component.ts
+++ 009.angular/angular-005-enrutado/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -32,7 +32,7 @@ export class RestaurantDetailComponent implements OnInit{
         location: "Calle corazón María 63",
         phone: "654789999",
         rating: 5,
-        imageUrl: "https://placehold.co/300"
+        imageUrl: "https://placehold.co/600"
       };
 
     // llamar al backend pidiendo el restaurante por el id
