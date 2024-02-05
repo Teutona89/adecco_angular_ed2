9bf717b85ed17aa94a34f15dd095207c19fda1c7
alansastre <alan@alansastre.co>
2024-02-05 12:29:12 +0100

    Angular Inyectar HttpClient en servicio y componente angular

--- 009.angular/angular-006-servicios/src/app/services/product.service.ts
+++ 009.angular/angular-006-servicios/src/app/services/product.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
 
+  // atributos
+
+  // constructor
+  constructor(private httpClient: HttpClient) { }
+
+  // metodos
   holaMundo(): string{
     return "Hola mundo";
   }
-
-
-  constructor() { }
 }
