baac974a2f1be10a6083effbe4bdc75bdae50eb8
alansastre <alan@alansastre.co>
2024-02-05 12:43:49 +0100

    Angular servicio crear método GET con httpClient

--- 009.angular/angular-006-servicios/src/app/services/product.service.ts
+++ 009.angular/angular-006-servicios/src/app/services/product.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../interfaces/product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
 
-  // atributos
-
-  // constructor
   constructor(private httpClient: HttpClient) { }
-
   // metodos
   holaMundo(): string{
     return "Hola mundo";
   }
+
+  obtenerProducto1(): Observable<Product> {
+    return this.httpClient.get<Product>('https://fakestoreapi.com/products/1');
+  }
+
 }
